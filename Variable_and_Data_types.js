c11246e28436a1de1b425ad9ce954a868273c051
Vaishnav-kumbhar <vk2222777@gmail.com>
2024-07-13 15:52:41 +0530

    first challenge completed

---
+++ Variable_and_Data_types.js
@@ -0,0 +1,41 @@
+// 1 activity
+// variable declaration
+var num = 10;
+console.log(num);
+let str = "hello";
+console.log(str);
+
+// 2 activity
+// Constant declaration
+const val = false;
+console.log(val);
+
+// 3 activity
+// data types
+const n = 5;
+const st = "js";
+const bool = true;
+const ob = {
+  height: 20,
+  weight: 30,
+};
+
+const ar = [1, 2, 3, 4, 5];
+
+console.log(typeof n);
+console.log(typeof st);
+console.log(typeof bool);
+console.log(typeof ob);
+console.log(typeof ar);
+
+// 4 activity
+// Reassigning Variables
+let init = 0;
+console.log(init);
+init = 1;
+console.log(init);
+
+// understanding const
+const mut = 1;
+mut = 5;
+console.log(mut);
