89f9a1c4a49a63fe57e4d1cc463f9f1dfb2a9d50
Vaishnav-kumbhar <vk2222777@gmail.com>
2024-07-15 10:17:08 +0530

    "operators completed day_2"

---
+++ operators.js
@@ -0,0 +1,26 @@
+// Artithmetic Operations
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+// Assignment Operators
+let num = 4;
+num += 2;
+console.log(num);
+num -= 2;
+console.log(num);
+
+// Comparison Operators
+console.log(9 > 5, 10 < 2);
+console.log(9 >= 9, 5 <= 8);
+console.log("9" === 5, 10 == 2);
+
+// Logical Operators
+if (10 >= 10 && 5 === 5) console.log("all good");
+if (10 >= 10 || 10 === 5) console.log(" good");
+if (!10 >= 5) console.log("true");
+
+// Ternary Operator
+10 > 15 ? console.log("true") : console.log("true");
