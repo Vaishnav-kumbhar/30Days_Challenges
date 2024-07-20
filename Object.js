9110c91d9a308bcc5439a7a88f2104e566b94a44
Vaishnav-kumbhar <vk2222777@gmail.com>
2024-07-20 12:51:01 +0530

    "object completed"

---
+++ Object.js
@@ -0,0 +1,43 @@
+const book = {
+  title: "time",
+  author: "vaishnav",
+  year: 2024,
+  fn: () => {
+    return `this is properties of book object ${book.title}, ${book.author}`;
+  },
+  update: function (year) {
+    this.year = year;
+  },
+
+  method: function () {
+    return `${this.title} book author name is ${this.author} it published in ${this.year}`;
+  },
+};
+
+console.log(book);
+const author = book.author;
+const title = book.title;
+console.log(title, author);
+console.log(book.fn());
+book.update(2005);
+console.log(book);
+
+const library = [
+  { title: "rich dad", year: 2004, author: "padman" },
+  { title: "mukti", year: 2012, author: "dinesh" },
+  { title: "sceince", year: 2006, author: "rahul" },
+  { title: "eco", year: 2007, author: "virat" },
+];
+
+console.log(library);
+const til = library.map((title) => title.title);
+console.log(til);
+const auth = library.map((author) => author.author);
+console.log(auth);
+console.log(book.method());
+
+for (const key in book) {
+  console.log(book[key]);
+}
+
+console.log(Object.keys(book), Object.values(book));
